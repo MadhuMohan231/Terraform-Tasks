c1d24d054f935bb35bcbecd4ed957c1ae28672cc
MadhuMohan231 <rkmadhu2310@gmail.com>
2024-04-25 14:03:15 +0530

    network stack

---
+++ VPC/variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {
+    type = string
+    description = "VPC CIDR value"
+    default = ""
+}
+
+variable "pub_sub_cidr" {
+    type = string
+    description = "Public-Subnet CIDR value"
+    default = ""
+}
+
+variable "pri_sub_cidr" {
+    type = string
+    description = "Private-Subnet CIDR values"
+    default = ""
+}
